c3aa88612737d08927d740733eefd46ba713a1cc
Vinay Rawat <vineyrawat@yahoo.com>
2022-01-13 05:02:35 +0000

    added sample cred file

---
+++ frappe_firebase/config/firebase_settings-sample.py
@@ -0,0 +1,13 @@
+def getCred():
+  return {
+  "type": "service_account",
+  "project_id": "frappefirebase",
+  "private_key_id": "--private-key-id--",
+  "private_key": "-----BEGIN PRIVATE KEY-----\n--private-key--\n-----END PRIVATE KEY-----\n",
+  "client_email": "--client-email--",
+  "client_id": "--client-id--",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "--client-x509-cert-url--"
+}
